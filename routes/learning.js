9f4ff3f30f302db37db5c3f64944db8adebfa88e
Isaiah <iwhj1@scms.waikato.ac.nz>
2017-03-09 16:19:22 +1300

    Added more stuff

--- routes/learning.js
+++ routes/learning.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+	res.render('learning/index', { title: 'Learning'})
+});
+
+router.get('/axis', function(req, res, next) {
+	res.render('learning/axis', { title: 'Learning'})
+});
+
 router.get('/curve', function(req, res, next) {
 	res.render('learning/curve', { title: 'Curve'} );
-})
+});
 
 module.exports = router;
