a82735d8745fad3a8e5b87898b5de1f8e1847a48
Isaiah <iwhj1@scms.waikato.ac.nz>
2017-03-10 17:05:53 +1300

    Updated

--- routes/learning.js
+++ routes/learning.js
@@ -17,8 +17,21 @@ router.get('/mesh', function(req, res, next) {
     res.render('learning/mesh', { title: 'Mesh'} );
 });
 
-router.get('/advance-word', function(req, res, next) {
-	res.render('learning/advanceWord.hbs', { title: 'Word' });
+router.get('/words', function(req, res, next) {
+	res.render('learning/words.hbs', { title: 'Word' });
 });
+
+router.get('/advance-word', function(req, res, next) {
+	res.render('learning/advanceWord.hbs', { title: 'Advance Word' });
+});
+
+router.get('/lights', function(req, res, next) {
+	res.render('learning/lights.hbs', { title: 'lights' });
+});
+
+router.get('/ironman', function(req, res, next) {
+	res.render('learning/ironman.hbs', { title: 'Iron Man' });
+});
+
 
 module.exports = router;
