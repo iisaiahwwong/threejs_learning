21fc7696f23deb80049747cdb342917317641ed2
Isaiah <iwhj1@scms.waikato.ac.nz>
2017-03-13 20:25:09 +1300

    Milestone 1

--- routes/custom.js
+++ routes/custom.js
@@ -9,4 +9,13 @@ router.get('/test/line', function(req, res, next) {
 	res.render('test/line', {title: 'Line'});
 });
 
+router.get('/test/arc', function(req, res, next) {
+	res.render('test/arc', {title: 'Arc'});
+});
+
+router.get('/test/label', function(req, res, next) {
+	res.render('test/label', {title: 'Label'});
+});
+
+
 module.exports = router;
