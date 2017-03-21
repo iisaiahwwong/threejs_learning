d6b3841b15d99ae34aeb8c8a398899da82eec315
iisaiahwwong <isaiah.wong@jirehsoho.com>
2017-03-22 09:13:36 +1300

    Update

--- routes/custom.js
+++ routes/custom.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+	res.render('custom/visual', {title: 'Visualisation'});
+});
+
 router.get('/audio', function(req, res, next) {
 	res.render('custom/audio', {title: 'Audio'});
 });
